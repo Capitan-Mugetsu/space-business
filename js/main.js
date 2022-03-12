b32e53cb12f16a66a99f873e08b0708590cd026a
Axror <axror.kurban@gmail.com>
2022-03-12 20:54:51 +0300

    create-slider

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 $(function(){
+  $('.slider__box').slick({
+    prevArrow: `<img  class="slider__arrow slider__arrow-left" src="images/left-arrow.svg">`,
+    nextArrow: `<img  class="slider__arrow slider__arrow-right" src="images/right-arrow.svg">`,
 
+  });
 });
